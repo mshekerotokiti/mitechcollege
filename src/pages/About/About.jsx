466534beb919eed4a531ec2ed995836e1fc46df2
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2025-05-21 17:14:44 +0300

    :styling: about page style

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './About.css';
 import ShortCourses from './ShortCourses';
-import Testimonials from './Testimonials';
 import WhyChooseUs from './WhyChooseUs';
 
 const About = () => {
@@ -12,12 +11,11 @@ const About = () => {
   }, []);
 
   return (
-    <section className="p-2" style={{ backgroundColor: "#D9D9D9", fontFamily: 'Roboto' ,fontSize: '35px'}}>
+    <section className="p-2" style={{ backgroundColor: "#D9D9D9", fontFamily: 'Roboto' ,fontSize: '25px'}}>
       <div className="container py-5">
         <div className="text-center mb-5">
           <h2 className="hover-underline">MITECH DRIVING SCHOOL & COMPUTER COLLEGE</h2>
           <p className="lead fst-italic text-secondary">Empowering Safe Drivers and Digital Experts</p>
-          {/* <hr className="mx-auto" /> */}
         </div>
         <div className="why-choose-section" data-aos="fade-right">
           <div className="container">
@@ -25,15 +23,6 @@ const About = () => {
           </div>
         </div>
         <ShortCourses />
-
-        <div className="bg-body-secondary py-3 rounded shadow-sm" data-aos="fade-left">
-          <Testimonials />
-        </div>
-
-        <div className="text-center mt-4" data-aos="zoom-in-up">
-          <h5 className="text-danger">Join us today and take the first step towards a safer, smarter future!</h5>
-          <a href="/contactus" className="btn b mt-3">Contact Us</a>
-        </div>
       </div>
     </section>
   );
