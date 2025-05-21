ba79792756013a7cdb5035172be50fee7aa11a4c
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2025-05-21 17:13:49 +0300

    :sparkles: course dropdown on navbar and styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
 import { NavLink } from 'react-router-dom';
-import  './Navbar.css';
+import logo from '../../assets/logo.png';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <>
       {/* Top Bar */}
-      <div className="bg- text-light bg-danger py-1 ">
+      <div className="bg-danger text-light py-1">
         <div className="container d-flex justify-content-between align-items-center medium">
           <span>Email: info@mitechcollege.com</span>
           <span>Call Us: +254 722522100</span>
@@ -15,11 +15,11 @@ const Navbar = () => {
       </div>
 
       {/* Main Navbar */}
-      <nav className="navbar navbar-expand text-dark bg-light sticky-top py-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top py-3">
         <div className="container">
           {/* Logo */}
           <NavLink className="navbar-brand py-2" to="/">
-          <img
+            <img
               src={logo}
               alt="Logo"
               style={{
@@ -29,7 +29,6 @@ const Navbar = () => {
               }}
             />
           </NavLink>
-
 
           {/* Toggler */}
           <button
@@ -45,54 +44,60 @@ const Navbar = () => {
           </button>
 
           {/* Nav Links & Search */}
-<div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-  {/* Nav Links */}
-  <ul className="navbar-nav mx-auto">
-    <li className="nav-item">
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) => isActive ? "nav-link active custom-nav-link" : "nav-link custom-nav-link"}
-      >
-        Home
-        <span className="underline"></span>
-      </NavLink>
-    </li>
-    <li className="nav-item dropdown">
-      <NavLink
-        to="/about"
-        className="nav-link dropdown-toggle custom-nav-link"
-        id="aboutDropdown"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        About
-        <span className="underline"></span>
-      </NavLink>
-      <ul className="dropdown-menu">
-        <li>
-          <NavLink to="/about" className="dropdown-item">
-            Why Mitech
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/faqs" className="dropdown-item">
-            FAQs
-          </NavLink>
-        </li>
-      </ul>
-    </li>
-    <li className="nav-item">
-      <NavLink
-        to="/contactus"
-        className={({ isActive }) => isActive ? "nav-link active custom-nav-link" : "nav-link custom-nav-link"}
-      >
-        Contact Us
-      </NavLink>
-    </li>
-  </ul>
+          <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul className="navbar-nav mx-auto fs-5">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link custom-nav-link">
+                  Home <span className="underline"></span>
+                </NavLink>
+              </li>
 
+              <li className="nav-item dropdown">
+                <NavLink
+                  to="/about"
+                  className="nav-link dropdown-toggle custom-nav-link"
+                  id="aboutDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  About <span className="underline"></span>
+                </NavLink>
+                <ul className="dropdown-menu">
+                  <li><NavLink to="/about" className="dropdown-item">Why Mitech</NavLink></li>
+                  <li><NavLink to="/faqs" className="dropdown-item">FAQs</NavLink></li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <NavLink
+                  to="/courses"
+                  className="nav-link dropdown-toggle custom-nav-link"
+                  id="coursesDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Courses
+                  <span className="underline"></span>
+                </NavLink>
+                <ul className="dropdown-menu">
+                  {/* <li><NavLink to="/courses" className="dropdown-item">All Courses</NavLink></li> */}
+                  <li><NavLink to="/courses/graphic-design" className="dropdown-item">Graphic Design</NavLink></li>
+                  <li><NavLink to="/courses/web-development" className="dropdown-item">Web Development</NavLink></li>
+                  <li><NavLink to="/courses/computer-packages" className="dropdown-item">Computer Packages</NavLink></li>
+                  <li><NavLink to="/courses/german-language" className="dropdown-item">German Language</NavLink></li>
+                  
+                </ul>
+              </li>
+
+
+              <li className="nav-item">
+                <NavLink to="/contactus" className="nav-link custom-nav-link">
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
 
             {/* Search Bar */}
             <form className="d-flex mt-3 mt-xl-0" role="search">
