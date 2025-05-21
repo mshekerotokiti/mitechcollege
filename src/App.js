2b2f2633ec64e2d0488e7eb965ff958b11870239
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2025-05-21 10:19:43 +0300

    :construction: routes structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer'; 
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import About from './pages/About/About';
+import ContactUs from './pages/Contact/ContactUs';
+
 
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <Footer />
-    </div>
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/contactus' element= {<ContactUs />} />
+        </Routes>
+
+        <Footer />
+      </div>
   );
 }
 
